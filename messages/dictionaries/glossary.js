034aaa1044c64b825f87eff988aab23b764f01ce
rsandstroem <rikard.sandstrom@gmail.com>
2017-12-04 12:04:32 +0000

    Add skeleton for the QnA dialog

---
+++ messages/dictionaries/glossary.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "Deloitte": {
+        "synonyms": [
+            "deloitte",
+            "deloitte sa",
+            "deloitte ag"
+        ],
+        "longText": "Der «Step Up» soll grundsätzlich sicherstellen, dass stille Reserven unter demjenigen Steuerregime besteuert werden, unter dem sie entstanden sind. \r\n\n Für Gesellschaften, die von einem privilegierten Status zur ordentlichen Besteuerung wechseln heisst dies, dass beim Statuswechsel vorhandene stille Reserven entsprechend tiefer besteuert werden sollen. Dies ist bei den Kantons- bzw. Gemeindesteuern relevant. Beim Step Up muss zwischen dem Step Up anlässlich eines vorzeitigen Statuswechsels (auch als altrechtlicher Step Up bezeichnet) und dem Step Up als Übergangsmassnahme (auch neurechtlicher Step Up genannt) unterschieden werden.",
+        "cards": [
+            "Step Up HL.jpg",
+            "Step Up HL 2.jpg",
+            "Step Up D.jpg",
+            "Step Up B.jpg",
+            "Step Up B 2.jpg",
+            "Step Up B 3.jpg",
+            "Step Up B 4.jpg"
+        ]
+    }
+}
